3c7c33b9479f68309fa9ec9a381f2120f742a7a2
Dilsha-Perera <dilsha.20220223@iit.ac.lk>
2024-03-17 14:14:08 +0530

    adding home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./componnents/Login";
 import Signup from "./componnents/Signup";
+import Home from './componnents/Home';
+
 function App() {
   return (
     <Router>
@@ -8,6 +10,8 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home/>}/>
+
         </Routes>
       </div>
     </Router>
