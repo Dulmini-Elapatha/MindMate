13ddbfecc212d4ad766df1497ee0e4f394cfbc20
Dilsha-Perera <dilsha.20220223@iit.ac.lk>
2024-03-23 13:28:47 +0530

    adding updated patientreg page

--- frontend/src/components/PatientReg.jsx
+++ frontend/src/components/PatientReg.jsx
@@ -47,7 +47,7 @@ const PatientReg = () => {
           />
         </div>
         <div>
-          <button type="submit">Register</button>
+        <a href="/Patient"><button type="submit">Register</button></a>
         </div>
       </form>
     </div>
