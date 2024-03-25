0c18415664b375e831ba824a499ae4c2716fde91
TharakaDine <138830891+TharakaDine@users.noreply.github.com>
2024-03-25 12:29:20 +0530

    updating appoinment view table

--- frontend/src/components/AppoinmentView.jsx
+++ frontend/src/components/AppoinmentView.jsx
@@ -1,31 +1,41 @@
-//AppointmentView.jsx
 import React from "react";
 import "./AppointmentView.css";
 import Navbar from "./LoginNavbar";
 import Footer from "./Footer";
 
-
-
-const AppointmentView = () => {
+const AppointmentView = ({ appointments, handleCancelBooking }) => {
   return (
     <div>
-      <Navbar/>
-      <table className=" View Appointment">
-      <thead>
-        <tr>
-          <th>Date </th>
-          <th>Time </th>
-          <th>Cancel Booking </th>
-          <th>Log Out</th>
-        </tr>
-      </thead>
-    </table>
-    <br/>
-    <br/>
-    <Footer/>
-    
+      <Navbar />
+      <table className="ViewAppointment">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Time</th>
+            <th>Cancel Booking</th>
+            <th>Log Out</th>
+          </tr>
+        </thead>
+        <tbody>
+          {appointments.map(appointment => (
+            <tr key={appointment.id}>
+              <td>{appointment.date}</td>
+              <td>{appointment.time}</td>
+              <td>
+                <button onClick={() => handleCancelBooking(appointment.id)}>
+                  Cancel
+                </button>
+              </td>
+              <td>
+                {/*  */}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Footer />
     </div>
   );
 };
 
-export default AppointmentView ;
+export default AppointmentView;
