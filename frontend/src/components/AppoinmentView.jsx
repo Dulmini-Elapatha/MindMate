cfb4eda8e08c95c2b8e497453c3e2c95bbb90359
TharakaDine <138830891+TharakaDine@users.noreply.github.com>
2024-03-25 12:22:54 +0530

    updating appoinment view jsx

---
+++ frontend/src/components/AppoinmentView.jsx
@@ -0,0 +1,31 @@
+//AppointmentView.jsx
+import React from "react";
+import "./AppointmentView.css";
+import Navbar from "./LoginNavbar";
+import Footer from "./Footer";
+
+
+
+const AppointmentView = () => {
+  return (
+    <div>
+      <Navbar/>
+      <table className=" View Appointment">
+      <thead>
+        <tr>
+          <th>Date </th>
+          <th>Time </th>
+          <th>Cancel Booking </th>
+          <th>Log Out</th>
+        </tr>
+      </thead>
+    </table>
+    <br/>
+    <br/>
+    <Footer/>
+    
+    </div>
+  );
+};
+
+export default AppointmentView ;
