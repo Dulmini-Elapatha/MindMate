0be458c7700e554f4aacab21ce15e1f18baecf10
TharakaDine <138830891+TharakaDine@users.noreply.github.com>
2024-04-03 23:21:14 +0530

    making errors

--- frontend/src/components/AppointmentForm.jsx
+++ frontend/src/components/AppointmentForm.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
+import './AppointmentForm.css';
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const AppointmentForm = ({ onSubmit }) => {
   const [patientName, setPatientName] = useState('');
   const [doctorName, setDoctorName] = useState('');
   const [appointmentDate, setAppointmentDate] = useState('');
+  const [appointmentTime, setAppointmentTime] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onSubmit({ patientName, doctorName, appointmentDate });
+    onSubmit({ doctorName, appointmentDate,appointmentTime });
     setPatientName('');
     setDoctorName('');
     setAppointmentDate('');
@@ -32,6 +36,12 @@ const AppointmentForm = ({ onSubmit }) => {
         placeholder="Appointment Date"
         value={appointmentDate}
         onChange={(e) => setAppointmentDate(e.target.value)}
+      />
+       <input
+        type="time"
+        placeholder="Appointment Time"
+        value={appointmentTime}
+        onChange={(e) => setAppointmentTime(e.target.value)}
       />
       <button type="submit">Submit</button>
     </form>
